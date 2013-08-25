aa3ee6d9bba63b7f81c38f49ee6b66580f54d6fe
Robins Gupta <robinskumar73@gmail.com>
2013-08-25 20:14:49 +0530

    Third Commit

--- model.js
+++ model.js
@@ -7,39 +7,114 @@ This script stores data information
 (function(window,Splash){
 //define Models here
 
-var Model = {};
+var Model = {
+	
+	//Define ERRORS HERE 
+	Error : { 
+		
+			argumentError : function(method_name){
+				try {
+						//Given method must be given an argument.
+						return "ArgumentError :" + method_name + "method must be given an argument.";
+				}
+				catch(e){
+					return "ArgumentError :method must be given an argument.";
+				}
+			}
+			
+	}
+};
 
+
+//Defigning Model datatype
 Model.datatype = {
 	
-	number:function(){
-		return typeof value === "number"? true : false;
+	//Prop
+	number:function(value){
+		return typeof value === "number" ? true : false;
 	},
+	
+	
 	
 	string:function(maxRange){
 		
 		var check = function(value){
+			
+			var flag = false;
 			if (maxRange && value.length > maxRange){
-				return false
+				return flag;
 			}
 			
 			if (typeof value === 'string'){
-				return true;
+				flag = true
+				
 			}			
-			else {
-				return false;
-			}
+			return flag;
 		}
 		
 		return check;
 	},
 	
 	
-	enum: function(){
-		if 
-		
-	}
-			
 	
+	enum: function(){
+		try{
+			if (arguments.length === 0){
+				throw(Model.Error.argumentError('Model.datatype.enum()'));
+			}
+		}
+		//Storing optional values
+		var possibleValues = Array.prototype.slice.call(arguments,0);
+		return function(value){
+			var flag = false;
+			if (!value){
+				//Argument Error
+				throw(Error[1]());
+			}
+			for (var i=0;i<possibleValues.length;i++){
+				if(value === possibleValues[i]){
+					flag = true;
+				}
+			}
+			//Return flag value
+			return flag;
+		};
+		
+	},
+	
+	
+	
+	//use it like prop
+	array: function(value){
+		
+		var flag=false;
+		if (typeof value === "object" && value instanceof Array)
+		{
+			flag=true
+		}
+		return flag;
+	},
+	
+	
+	//custom should accept a custom datatype function as argument and returns boolean
+	//fn would accept an value argument and must return a boolean
+	custom: function(fn){
+		return fn;
+	}
+		
+};
+
+
+
+Model.constraint = {
+	
+	Null : true,
+	PrimaryKey : true,
+	
+	//Foreign Key
+	
+	
+};
 
 
 
